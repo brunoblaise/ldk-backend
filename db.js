623cf2f88acc3891ee6cd47624b7a1eea4c8197c
brunoblaise <brunoblaise94@gmail.com>
2022-11-27 19:34:53 +0200

    worl

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-const devConfig =`postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${ PGDATABASE }`;
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+
 const proConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Pool({
